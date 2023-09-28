6ddf492d2e483e1da71cdc2eb2c30e0ea359eb90
nathan hensel <nathanihensel@gmail.com>
2023-09-27 22:23:23 -0500

    daemon control loop

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,14 +1,40 @@
 package daemon
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
 )
+
+const defaultTick = 1 * time.Second
+
+func run(ctx context.Context, out io.Writer) error {
+	log.SetOutput(out)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.Tick(defaultTick):
+			log.Printf("tick")
+		}
+	}
+}
 
 func Main(cidr string) error {
 	fmt.Println("daemon got cidr", cidr)
 
-	err := createbr()
-	if err != nil {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	defer func() {
+		cancel()
+	}()
+
+	if err := run(ctx, os.Stdout); err != nil {
 		return err
 	} else {
 		return nil
